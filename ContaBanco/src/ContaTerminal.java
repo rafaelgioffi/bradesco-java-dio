42947708a7a77b081c528df8af3e02e86ab509f1
Rafael Gioffi <rafaelgioffi@gmail.com>
2025-02-08 23:25:18 -0300

    Entrega do desafio

--- ContaBanco/src/ContaTerminal.java
+++ ContaBanco/src/ContaTerminal.java
@@ -1,11 +1,34 @@
+import java.text.NumberFormat;
+import java.util.Locale;
 import java.util.Scanner;
 
 public class ContaTerminal {
     public static void main(String[] args) throws Exception {
         int numero;
-        String conta, nomeCliente;
-        double saldo;
+        String agencia, nomeCliente, saldoFormat;
+        float saldo;
+        NumberFormat format;
 
-        Scanner numero = "Qual número da conta?";
+        Scanner sc = new Scanner(System.in);
+        sc.useLocale(Locale.forLanguageTag("pt-BR"));
+
+        System.err.print("Qual número da conta? ");
+        numero = sc.nextInt();
+        
+        System.out.print("Qual número da agência (com dígito)? ");
+        agencia = sc.next();
+        
+        System.out.print("Qual seu nome? ");
+        nomeCliente = sc.next();
+        
+        System.out.print("Quanto deseja depositar inicialmente? ");
+        saldo = sc.nextFloat();
+
+        format = NumberFormat.getNumberInstance(new Locale("pt","BR"));
+        saldoFormat = format.format(saldo);
+
+        new ProcessBuilder("cmd", "/c", "cls").inheritIO().start().waitFor();
+
+        System.err.println("Olá " + nomeCliente + ", obrigado por criar uma conta em nosso banco, sua agência é " + agencia + ", conta " + numero + " e seu saldo de R$" + saldoFormat + " já está disponível para saque.");
     }
 }
