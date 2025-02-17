2ae3c40004a7c0322d7befb3e9ac3d7ebbbaea58
Rafael Gioffi <rafaelgioffi@gmail.com>
2025-02-16 21:50:48 -0300

    Interfaces...

---
+++ ServicosDigitalizacao/src/equipamentos/digitalizadora/Digitalizadora.java
@@ -0,0 +1,11 @@
+package equipamentos.digitalizadora;
+
+public interface Digitalizadora {
+    public void digitalizar();
+/*
+    {
+  public void digitaliza() {
+//        System.out.println("DIGITALIZANDO...");
+}
+*/
+}
---
+++ ServicosDigitalizacao/src/equipamentos/impressora/Impressora.java
@@ -0,0 +1,10 @@
+package equipamentos.impressora;
+
+public interface Impressora {
+    public void imprimir();
+/*
+    public void imprimme() {
+        System.out.println("IMPRIMINDO...");
+    }
+ */
+}
