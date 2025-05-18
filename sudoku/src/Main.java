7c05621ac196693d9ccbdfa9c1011bce08881bec
Rafael Gioffi <rafaelgioffi@gmail.com>
2025-05-18 11:58:22 -0300

    Desafio Sudoku 1.4

--- sudoku/src/Main.java
+++ sudoku/src/Main.java
@@ -4,6 +4,10 @@ import org.w3c.dom.ls.LSOutput;
 import java.lang.reflect.Array;
 import java.util.Random;
 import java.util.Scanner;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+import static java.util.stream.Collectors.toMap;
 
 public class Main {
     public static final String VERDE = "\u001B[32m";
@@ -14,6 +18,15 @@ public class Main {
     private final static int BOARD_LIMIT = 9;
 
     public static void main(String[] args) {
+        final var positions = Stream.of(args)
+                .collect(toMap(
+                        k -> k.split(";")[0],
+                        v -> v.split(";")[1]
+                ));
 
+        var option = -1;
+        while (true) {
+
+        }
     }
 }
