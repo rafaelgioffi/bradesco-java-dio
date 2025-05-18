cdb83dcfbca6254d39197baf12b69721da6fad76
Rafael Gioffi <rafael.gioffi@atos.net>
2025-05-18 16:24:10 -0300

    Sudoku 1.4

--- sudoku/src/Main.java
+++ sudoku/src/Main.java
@@ -12,7 +12,7 @@ import static java.util.stream.Collectors.toMap;
 public class Main {
     public static final String VERDE = "\u001B[32m";
     public static final String RESET = "\u001B[0m";
-    Scanner sc = new Scanner(System.in);
+    static Scanner sc = new Scanner(System.in);
 
     private static Board board;
     private final static int BOARD_LIMIT = 9;
@@ -26,7 +26,25 @@ public class Main {
 
         var option = -1;
         while (true) {
+            System.out.println("Escolha uma opção");
+            System.out.println("1 - Iniciar Novo jogo");
+            System.out.println("2 - Inserir número");
+            System.out.println("3 - Remover Número");
+            System.out.println("4 - Visualizar Jogo Atual");
+            System.out.println("5 - Verificar Status do Jogo");
+            System.out.println("6 - Limpar Jogo");
+            System.out.println("7 - Finalizar Jogo");
+            System.out.println("0 - Sair");
 
+            option = sc.nextInt();
+
+            switch (option) {
+                case 1 -> StartGame(positions);
+                case 2 -> InputNumber();
+                case 3 -> RemoveNumber();
+                case 0 -> System.exit(0);
+                default -> System.out.println("Opção inválida!");
+            }
         }
     }
 }
