50092214ec53d72617cd001a35ce3340dc8a1bb2
Rafael Gioffi <rafaelgioffi@gmail.com>
2025-05-18 23:55:37 -0300

    Desafio Sudoku 1.5

--- sudoku/src/Main.java
+++ sudoku/src/Main.java
@@ -1,13 +1,16 @@
+import br.com.dio.util.BoardTemplate;
 import br.com.dio.util.model.Board;
-import org.w3c.dom.ls.LSOutput;
+import br.com.dio.util.model.Space;
 
-import java.lang.reflect.Array;
-import java.util.Random;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
 import java.util.Scanner;
-import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
-import static java.util.stream.Collectors.toMap;
+import static br.com.dio.util.BoardTemplate.BOARD_TEMPLATE;
+import static java.util.Objects.*;
+import static java.util.stream.Collectors.*;
 
 public class Main {
     public static final String VERDE = "\u001B[32m";
@@ -39,13 +42,156 @@ public class Main {
             option = sc.nextInt();
 
             switch (option) {
-                case 1 -> StartGame(positions);
-                case 2 -> InputNumber();
-                case 3 -> RemoveNumber();
-                case 4 -> ViewGame();
+                case 1 -> startGame(positions);
+                case 2 -> inputNumber();
+                case 3 -> removeNumber();
+                case 4 -> viewGame();
+                case 5 -> statusGame();
+                case 6 -> clearGame();
+                case 7 -> finishGame();
                 case 0 -> System.exit(0);
                 default -> System.out.println("Opção inválida!");
             }
         }
     }
+
+    private static void startGame(final Map<String, String> positions) {
+        if (nonNull(board)) {
+            System.out.println("O jogo já foi iniciado.");
+            return;
+        }
+
+        List<List<Space>> spaces = new ArrayList<>();
+        for (int i = 0; i < BOARD_LIMIT; i++) {
+            spaces.add(new ArrayList<>());
+            for (int j = 0; j < BOARD_LIMIT; j++) {
+                var positionConfig = positions.get("%s,%s".formatted(i, j));
+                var expected = Integer.parseInt(positionConfig.split(",")[0]);
+                var fixed = Boolean.parseBoolean(positionConfig.split(",")[1]);
+                var currentSpace = new Space(expected, fixed);
+                spaces.get(i).add(currentSpace);
+            }
+        }
+        board = new Board(spaces);
+        System.out.println("O jogo está pronto para começar! Boa sorte!");
+    }
+
+    private static void inputNumber() {
+        if (isNull(board)) {
+            System.out.println("O jogo ainda não foi iniciado...");
+            return;
+        }
+
+        System.out.print("Informe a COLUNA que deseja inserir o número: ");
+        var col = runUntilGetValidNumber(0, BOARD_LIMIT - 1);
+        System.out.print("Informe a LINHA que deseja inserir o número: ");
+        var row = runUntilGetValidNumber(0, BOARD_LIMIT - 1);
+
+        System.out.printf("Informe o NÚMERO que será inserido na posição [%s]X[%s]: ", col, row);
+        var value = runUntilGetValidNumber(1, BOARD_LIMIT - 1);
+
+        if (!board.changeValue(col, row, value)) {
+            System.out.printf("A posição [%s]X[%s] tem um valor fixo, favor selecionar outra posição...", col, row);
+            return;
+        }
+    }
+
+    private static void removeNumber() {
+        if (isNull(board)) {
+            System.out.println("O jogo ainda não foi iniciado...");
+            return;
+        }
+
+        System.out.print("Informe a COLUNA que deseja inserir o número: ");
+        var col = runUntilGetValidNumber(0, BOARD_LIMIT - 1);
+        System.out.print("Informe a LINHA que deseja inserir o número: ");
+        var row = runUntilGetValidNumber(0, BOARD_LIMIT - 1);
+
+        if (!board.clearValue(col, row)) {
+            System.out.printf("A posição [%s]X[%s] tem um valor fixo, favor selecionar outra posição...", col, row);
+            return;
+        }
+    }
+
+    private static void viewGame() {
+        if (isNull(board)) {
+            System.out.println("O jogo ainda não foi iniciado...");
+            return;
+        }
+
+        var args = new Object[81];
+        var argPos = 0;
+
+        for (int i = 0; i < BOARD_LIMIT; i++) {
+            for (var col: board.getSpaces()) {
+                args[argPos ++] = " " + ((isNull(col.get(i).getActual())) ? " " : col.get(i).getActual());
+            }
+        }
+        System.out.println("Seu jogo se encontra da seguinte forma");
+        System.out.printf((BOARD_TEMPLATE) + "\n", args);
+    }
+
+    private static void statusGame() {
+        if (isNull(board)) {
+            System.out.println("O jogo ainda não foi iniciado...");
+            return;
+        }
+
+        System.out.printf("O jogo atualmente se encontra no status %s\n", board.getStatus().getLabel());
+
+        if (board.hasErrors()) {
+            System.out.println("O jogo contém erros...");
+        }
+        else {
+            System.out.println("Jogo não contém erros.");
+        }
+    }
+
+    private static void clearGame() {
+        if (isNull(board)) {
+            System.out.println("O jogo ainda não foi iniciado...");
+            return;
+        }
+
+        System.out.println("Tem certeza que deseja limpar o jogo? Todo progresso será perdido! (S/N)");
+        var confirm = sc.next();
+        while (!confirm.equalsIgnoreCase("s") || !confirm.equalsIgnoreCase("n")) {
+            System.out.println("Resposta inválida!\ns para Sim\nn para Não");
+            confirm = sc.next();
+        }
+
+        if (confirm.equalsIgnoreCase("s")) {
+            board.reset();
+            System.out.println("O jogo foi limpo com sucesso!");
+        }
+    }
+
+    private static void finishGame() {
+        if (isNull(board)) {
+            System.out.println("O jogo ainda não foi iniciado...");
+            return;
+        }
+
+        if (board.gameIsFinished()) {
+            System.out.println("Parabéns! Você concluiu o jogo!");
+            viewGame();
+            board = null;
+        }
+        else if (board.hasErrors()) {
+            System.out.println("O jogo não contém erros! Verifique e ajuste antes de finalizar.");
+        }
+        else {
+            System.out.println("O jogo ainda não pode ser encerrado... Favor preencha todos os espaços!");
+        }
+    }
+
+    private static int runUntilGetValidNumber(final int min, final int max) {
+        var current = sc.nextInt();
+        while (current < min || current > max) {
+            System.out.printf("Número inválido! Escolha um número entre %s e %s... Tente novamente.", min, max);
+            current = sc.nextInt();
+        }
+        return current;
+    }
+
 }
--- sudoku/src/br/com/dio/util/model/GameStatusEnum.java
+++ sudoku/src/br/com/dio/util/model/GameStatusEnum.java
@@ -1,7 +1,17 @@
 package br.com.dio.util.model;
 
 public enum GameStatusEnum {
-    NON_STARTED,
-    INCOMPLETE,
-    COMPLETED;
+    NON_STARTED("não iniciado"),
+    INCOMPLETE("incompleto"),
+    COMPLETED("completo");
+
+    private String label;
+
+    GameStatusEnum(final String label) {
+        this.label = label;
+    }
+
+    public String getLabel() {
+        return label;
+    }
 }
