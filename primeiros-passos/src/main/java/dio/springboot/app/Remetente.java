fc93eb7c7bd6387bbbd63cba9d4c4c156dc6accc
Rafael Gioffi <rafaelgioffi@gmail.com>
2025-05-21 23:41:31 -0300

    SpringBoot 2.7

---
+++ primeiros-passos/src/main/java/dio/springboot/app/Remetente.java
@@ -0,0 +1,42 @@
+package dio.springboot.app;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.stereotype.Component;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+@Configuration
+@ConfigurationProperties(prefix = "remetente")
+public class Remetente {
+    private String nome;
+    private String email;
+    private List<Long> telefones = new ArrayList<>(Arrays.asList());
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public List<Long> getTelefones() {
+        return telefones;
+    }
+
+    public void setTelefones(List<Long> telefones) {
+        this.telefones = telefones;
+    }
+}
--- primeiros-passos/src/main/java/dio/springboot/app/SistemaMensagem.java
+++ primeiros-passos/src/main/java/dio/springboot/app/SistemaMensagem.java
@@ -1,5 +1,6 @@
 package dio.springboot.app;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
@@ -10,18 +11,20 @@ import java.util.List;
 
 @Component
 public class SistemaMensagem implements CommandLineRunner {
-    @Value("${name:NoReply-DIO}")
-    private String nome;
-    @Value("${email}")
-    private String email;
-    @Value("${telefones}")
-    private List<Long> telefones = new ArrayList<>(Arrays.asList());
+//    @Value("${name:NoReply-DIO}")
+//    private String nome;
+//    @Value("${email}")
+//    private String email;
+//    @Value("${telefones}")
+//    private List<Long> telefones = new ArrayList<>(Arrays.asList());
+    @Autowired
+    private Remetente remetente;
 
     @Override
     public void run(String... args) throws Exception {
-        System.out.println("Mensagem enviada por: " + nome
-        + "\nE-mail: " + email
-        + "\ncom os telefones: " + telefones);
+        System.out.println("Mensagem enviada por: " + remetente.getNome()
+        + "\nE-mail: " + remetente.getEmail()
+        + "\ncom os telefones: " + remetente.getTelefones());
 
         System.out.println("Seu cadastro foi aprovado!");
     }
